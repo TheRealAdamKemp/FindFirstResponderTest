b5c480c10f7769d4e9582bf4f74a7bf188de96e4
Adam Kemp <adam@adamkemp.com>
2015-04-29 22:55:39 -0500

    First commit.

---
+++ FindFirstResponderTest/ResponderUtils.cs
@@ -0,0 +1,44 @@
+﻿using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace FindFirstResponderTest
+{
+    /// <summary>
+    /// Utilities for UIResponder.
+    /// </summary>
+    public static class ResponderUtils
+    {
+        internal const string GetResponderSelectorName = "AK_findFirstResponder:";
+        private static readonly Selector GetResponderSelector = new Selector(GetResponderSelectorName);
+
+        /// <summary>
+        /// Gets the current first responder if there is one.
+        /// </summary>
+        /// <returns>The first responder if one was found or null otherwise..</returns>
+        public static UIResponder GetFirstResponder()
+        {
+            using (var result = new GetResponderResult())
+            {
+                UIApplication.SharedApplication.SendAction(GetResponderSelector, target: null, sender: result, forEvent: null);
+                return result.FirstResponder;
+            }
+        }
+
+        internal class GetResponderResult : NSObject
+        {
+            public UIResponder FirstResponder { get; set; }
+        }
+    }
+
+    [Category(typeof(UIResponder))]
+    internal static class FindFirstResponderCategory
+    {
+        [Export(ResponderUtils.GetResponderSelectorName)]
+        public static void GetResponder(this UIResponder responder, ResponderUtils.GetResponderResult result)
+        {
+            result.FirstResponder = responder;
+        }
+    }
+}
+
---
+++ FindFirstResponderTest/ViewController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace FindFirstResponderTest
+{
+    public partial class ViewController : UIViewController
+    {
+        private NSObject _keyboardWillShowNotification;
+        private NSObject _keyboardWillHideNotification;
+
+        private UITextField _focusedField;
+        private UIColor _originalColor;
+        private UIView _tapView;
+
+        public ViewController(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            _tapView = new UIView();
+            _tapView.AddGestureRecognizer(new UITapGestureRecognizer(() => View.EndEditing(force: true)));
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+
+            _keyboardWillShowNotification = UIKeyboard.Notifications.ObserveWillShow((s, e) =>
+                {
+                    _focusedField = ResponderUtils.GetFirstResponder() as UITextField;
+                    if (_focusedField != null)
+                    {
+                        _originalColor = _focusedField.BackgroundColor;
+                        _focusedField.BackgroundColor = UIColor.LightGray;
+
+                        View.AddSubview(_tapView);
+                    }
+                });
+
+            _keyboardWillHideNotification = UIKeyboard.Notifications.ObserveWillHide((s, e) =>
+                {
+                    if (_focusedField != null)
+                    {
+                        _focusedField.BackgroundColor = _originalColor;
+                        _focusedField = null;
+                        _originalColor = null;
+
+                        _tapView.RemoveFromSuperview();
+                    }
+                });
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+
+            NSNotificationCenter.DefaultCenter.RemoveObserver(_keyboardWillShowNotification);
+            NSNotificationCenter.DefaultCenter.RemoveObserver(_keyboardWillHideNotification);
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+
+            _tapView.Frame = View.Bounds;
+        }
+    }
+}
+
